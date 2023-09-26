b33feb32263c1e60b865335d0983a32aa66f0a28
Varg-Vikernes01~ <vikernesvarg058@gmail.com>
2023-09-25 23:11:10 -0600

    efecto transition

--- src/Components/AboutProuct/descriptionProduct.jsx
+++ src/Components/AboutProuct/descriptionProduct.jsx
@@ -9,7 +9,7 @@ const DescriptionProduct = () => {
                 <div className="bg-gray-200 rounded-lg p-2">
                     <img
                         className="w-full h-auto rounded-lg"
-                        src="https://via.placeholder.com/800x400"  // Cambia la URL de la imagen y el tamaño
+                        src="https://www.k9ofmine.com/wp-content/uploads/2020/09/oatmeal-bath-for-dogs-768x512.jpg"  // Cambia la URL de la imagen y el tamaño
                         alt="Imagen del producto"
                     />
                 </div>
@@ -17,16 +17,20 @@ const DescriptionProduct = () => {
 
             {/* Columna derecha con el contenido */}
             <div className="w-1/2 ml-4">
-                <h2 className="text-2xl font-bold mb-2 animate-bounce">Título del Producto</h2>
-                <p className="text-gray-700 mb-4 animate-fade-in">
-                    Información sobre el producto que puede ser más larga si es necesario.
-                </p>
-                {/* Enlace al componente AboutProduct */}
-                <Link to="/about-product">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                        Conoce Más sobre nuestro Producto
-                    </button>
-                </Link>
+                <div className="animate-slide-left">
+                    <h2 className="text-2xl font-bold mb-2">Sobre nuestro producto</h2>
+                    <p className="text-gray-700 mb-4">
+                        <br />
+                        "Nuestro queso vegano untable es una deliciosa alternativa a los productos lácteos tradicionales. Elaborado con leche vegetal, agentes de espesamiento y cultivos probióticos, este queso ofrece una textura y sabor excepcionales. Está diseñado especialmente para personas vegetarianas adultas de 25-70 años, intolerantes a la lactosa y con problemas intestinales, así como para cualquier persona que busque una opción saludable y deliciosa. Nuestra marca, representada por colores frescos y naturales, refleja la calidad y frescura de nuestro producto. Además, el imagotipo con los colores de la bandera mexicana enfatiza su origen nacional y su conexión con la tradición culinaria mexicana."
+                        <br /><br />
+                    </p>
+                    {/* Enlace al componente AboutProduct */}
+                    <Link to="/about-product">
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+                            Conoce Más sobre nuestro Producto
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- src/Components/AboutProuct/descriptionProductRight.jsx
+++ src/Components/AboutProuct/descriptionProductRight.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'; // Importa Link para crear un enlace in
 
 const DescriptionProductRight = () => {
     return (
-        <div className="bg-white shadow-lg rounded-lg p-4 m-4 max-w-screen-xl mx-auto flex overflow-hidden">
+        <div className="bg-white shadow-lg rounded-lg p-4 m-4 max-w-screen-xl mx-auto flex">
             {/* Columna izquierda con el contenido */}
-            <div className="w-1/2 overflow-hidden">
+            <div className="w-1/2 mr-4">
                 <div className="animate-slide-left">
                     <h2 className="text-2xl font-bold mb-2">Título del Producto</h2>
                     <p className="text-gray-700 mb-4">
@@ -25,7 +25,7 @@ const DescriptionProductRight = () => {
                 <div className="bg-gray-200 rounded-lg p-2">
                     <img
                         className="w-full h-auto rounded-lg"
-                        src="https://via.placeholder.com/800x400"  // Cambia la URL de la imagen y el tamaño
+                        src="https://thefoodieandthefix.com/wp-content/uploads/2016/02/DSC_5125-9-750x500.jpg"  // Cambia la URL de la imagen y el tamaño
                         alt="Imagen del producto"
                     />
                 </div>
