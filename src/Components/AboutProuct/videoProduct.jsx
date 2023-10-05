0d9cba29bcc6953e267850d98079e93e734a9ddd
Varg-Vikernes01~ <vikernesvarg058@gmail.com>
2023-10-04 23:12:01 -0600

    edit del video

--- src/Components/AboutProuct/videoProduct.jsx
+++ src/Components/AboutProuct/videoProduct.jsx
@@ -3,14 +3,14 @@ import fondoVideo from './fondodelvideo.png';
 
 const VideoProduct = () => {
     return (
-        <div
-            className="bg-cover bg-center h-96 md:h-screen flex items-center justify-center"
+        <div className="bg-cover bg-center h-96 md:h-screen flex flex-col items-center justify-center"
             style={{
                 backgroundImage: `url(${fondoVideo})`, // Reemplaza con la ruta correcta de tu imagen de fondo
             }}
         >
+            <h1 className="text-5xl text-white font-bold mb-6 text-center">Te invitamos a conocer más sobre nuestro producto, mirando el siguiente video</h1>
             <iframe
-                className="w-1/2 h-1/2" // Aquí ajustamos el tamaño del video a la mitad
+                className="w-2/3 h-2/3" // Ajustamos el tamaño del video a dos tercios del contenedor
                 src="https://www.youtube.com/embed/tgbNymZ7vqY" // Reemplaza con la URL de tu video
                 title="Video del Producto"
                 allowFullScreen
