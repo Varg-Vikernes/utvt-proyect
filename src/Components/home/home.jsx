1ba8ed5dcde23c90ada4720c136dac9126f82106
Darkneko0 <al221911729@gmail.com>
2023-10-06 18:01:25 -0600

    Feat(home, Navbar)
    - Cambios de las rutas en el Nabvar
    - Colocación de id
    - Adiccion del Logo

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -1,40 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const Home = ({ backgroundImage, height, width }) => {
-    const containerStyle = {
-        minHeight: height || '70vh',
-        width: width || '100%',
-        backgroundImage: `url(${backgroundImage || require("../../Components/home/fondo.png")})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo transparente
-    };
+  const containerStyle = {
+    minHeight: height || "70vh",
+    width: width || "100%",
+    backgroundImage: `url(${
+      backgroundImage || require("../../Components/home/fondo.png")
+    })`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo transparente
+  };
 
-    const textStyle = {
-        color: "white", // Color del texto en blanco
-    };
+  const textStyle = {
+    color: "white", // Color del texto en blanco
+  };
 
-    return (
-        <div style={containerStyle} className="flex justify-center items-center flex-col text-center">
-            {/* Mensaje de Bienvenida */}
-            <h1 className="text-4xl font-bold" style={textStyle}>
-                ¡Bienvenido a nuestra aplicación!
-            </h1>
-            <h3 className="text-xl mt-4" style={textStyle}>
-                Descubre lo que podemos ofrecerte.
-            </h3>
+  return (
+    <div
+      style={containerStyle}
+      className="flex justify-center items-center flex-col text-center"
+      id="bienvenida"
+    >
+      {/* Mensaje de Bienvenida */}
+      <h1 className="text-4xl font-bold" style={textStyle}>
+        ¡Bienvenido a nuestra aplicación!
+      </h1>
+      <h3 className="text-xl mt-4" style={textStyle}>
+        Descubre lo que podemos ofrecerte.
+      </h3>
 
-            {/* Botones */}
-            <div className="mt-4 space-x-4">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
-                    Reproducir Video
-                </button>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
-                    Conócenos
-                </button>
-            </div>
-        </div>
-    );
+      {/* Botones */}
+      <div className="mt-4 space-x-4">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+          Reproducir Video
+        </button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+          Conócenos
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/navbar/nav.jsx
+++ src/Components/navbar/nav.jsx
@@ -1,76 +1,97 @@
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
-    const location = useLocation();
+  const location = useLocation();
 
-    const navbarStyle = {
-        background: 'rgb(255,255,255)',
-        background: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(247,249,210,1) 0%, rgba(255,255,255,0.9332107843137255) 100%)',
-        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-    };
+  const navbarStyle = {
+    background: "rgb(255,255,255)",
+    background:
+      "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(247,249,210,1) 0%, rgba(255,255,255,0.9332107843137255) 100%)",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+  };
 
-    const menuLinkStyle = "text-black hover:text-blue-400";
-    const buttonStyle = "bg-blue-800 text-white px-4 py-2 rounded-md mr-2 hover:bg-blue-600";
+  const menuLinkStyle = "text-black hover:text-blue-400";
+  const buttonStyle =
+    "bg-blue-800 text-white px-4 py-2 rounded-md mr-2 hover:bg-blue-600";
 
-    return (
-        <nav style={navbarStyle} className="p-4 shadow-lg sticky top-0 z-50">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center space-x-6">
-                    <div className="text-black font-bold text-xl">UTVT Proyecto</div>
-                </div>
-                <div className="flex items-center space-x-6">
-                    <a
-                        className={location.pathname === '/home' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/home')}
-                    >
-                        Inicio
-                    </a>
-                    <a
-                        className={location.pathname === '/porque' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/porque')}
-                    >
-                        Nuestro producto
-                    </a>
-                    <a
-                        className={location.pathname === '/opcion1' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/opcion1')}
-                    >
-                        Surgimiento
-                    </a>
-                    <a
-                        className={location.pathname === '/opcion2' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/opcion2')}
-                    >
-                        Elaboracion
-                    </a>
-                    <a
-                        className={location.pathname === '/opcion3' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/opcion3')}
-                    >
-                        Recetario
-                    </a>
-                    <a
-                        className={location.pathname === '/opcion3' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/opcion3')}
-                    >
-                        Conocenos
-                    </a>
-                    <a
-                        className={location.pathname === '/opcion3' ? `${menuLinkStyle} border-b-2 border-blue-500` : menuLinkStyle}
-                        onClick={() => navigate('/opcion3')}
-                    >
-                        Blog
-                    </a>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <button className={buttonStyle} onClick={() => navigate('/login')}>Iniciar Sesión</button>
-                    <button className={buttonStyle} onClick={() => navigate('/register')}>Registrar</button>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav style={navbarStyle} className="p-4 shadow-lg sticky top-0 z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-6">
+          <div className="text-black font-bold text-xl">
+            <img
+              src="../assets/home/navbar/logo q-spicy.jpeg"
+              alt="Logo de Q-spicy"
+              className=" h-auto object-cover max-w-14 max-h-14"
+            />
+          </div>
+        </div>
+        <div className="flex items-center space-x-6">
+          <a
+            className={
+              location.pathname === "/home#bienvenida"
+                ? `${menuLinkStyle} border-b-2 border-blue-500`
+                : menuLinkStyle
+            }
+            onClick={() => navigate("/home#bienvenida")}
+          >
+            Bienvenido
+          </a>
+          <a
+            className={
+              location.pathname === "/home#como-surgio-esta-idea"
+                ? `${menuLinkStyle} border-b-2 border-blue-500`
+                : menuLinkStyle
+            }
+            onClick={() => navigate("/home#como-surgio-esta-idea")}
+          >
+            ¿Cómo Surgio?
+          </a>
+          <a
+            className={
+              location.pathname === "/home#conocenos"
+                ? `${menuLinkStyle} border-b-2 border-blue-500`
+                : menuLinkStyle
+            }
+            onClick={() => navigate("/home#conocenos")}
+          >
+            Conocenos
+          </a>
+          <a
+            className={
+              location.pathname === "/home#recetario"
+                ? `${menuLinkStyle} border-b-2 border-blue-500`
+                : menuLinkStyle
+            }
+            onClick={() => navigate("/home#recetario")}
+          >
+            Recetario
+          </a>
+
+          <a
+            className={
+              location.pathname === "/blog"
+                ? `${menuLinkStyle} border-b-2 border-blue-500`
+                : menuLinkStyle
+            }
+            onClick={() => navigate("/blog")}
+          >
+            Blog
+          </a>
+        </div>
+        <div className="flex items-center space-x-2">
+          <button className={buttonStyle} onClick={() => navigate("/login")}>
+            Iniciar Sesión
+          </button>
+          <button className={buttonStyle} onClick={() => navigate("/register")}>
+            Registrar
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
