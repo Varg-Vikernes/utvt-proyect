98123b54af23a870c90f64dd026a489b59f37858
Varg-Vikernes1~ <vikernesvarg058@gmail.com>
2023-10-20 21:59:05 -0600

    recetas

--- src/Components/home/recipes/RecipeCards.jsx
+++ src/Components/home/recipes/RecipeCards.jsx
@@ -32,18 +32,19 @@ class RecipeCards extends Component {
 
     const { recetas, showDetails } = this.state;
     return (
-      <div>
+      <div className="mx-4"> {/* Agregar margen a los lados */}
         <h1 className="text-3xl font-bold text-center mb-4">Recetas</h1>
         <p className="text-xl text-gray-600 text-center mb-4">Descubre deliciosas recetas para tu cocina</p>
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {recetas.map((receta) => (
             <li key={receta.idReceta} className={cardStyle}>
+              <img src="https://th.bing.com/th/id/OIP.U0hKvSgFM_k2wdWVCFxeZAHaFj?pid=ImgDet&rs=1" alt="Imagen de la receta" className="mb-2 w-100 h-100" />
               <h2 className="text-xl font-semibold mb-2">{receta.titulo}</h2>
               <p>Descripción: {receta.descripcion}</p>
               {showDetails && (
                 <div className="mt-4">
-                  <p>Elaboración: {receta.elaboracion}</p>
                   <p>Ingredientes: {receta.ingredientes}</p>
+                  <p>Elaboración: {receta.elaboracion}</p>
                 </div>
               )}
               <p>Actualizado en: {receta.updatedAt}</p>
