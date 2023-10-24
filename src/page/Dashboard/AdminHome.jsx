9bcb888e8b186bfdea81cdc27a4aa059acc424c9
Valentin <valentinmartinez1103@gmail.com>
2023-10-24 12:45:11 -0600

    Se corrigio el nombre de extincion del archivo.

--- src/page/Dashboard/AdminHome.jsx
+++ src/page/Dashboard/AdminHome.jsx
@@ -3,8 +3,8 @@ import SideBarComponent from "../../Components/admin/SideBarComponent";
 import Usuarios from "../../Components/admin/User/Usuarios";
 import Recetas from "../../Components/admin/Recipes/Recetas";
 import Blog from "../../Components/admin/Blog/BlogAdmin";
-import NavbarAdmin from "../../Components/navbar/navAdmin";
-import { formStyles, responsiveFormStyles } from "../../styles/Constants";
+
+
 
 function AdminHome() {
   const [selectedOption, setSelectedOption] = useState("");
@@ -15,21 +15,21 @@ function AdminHome() {
 
   return (
     <div >
-    <NavbarAdmin />
-    <div className="flex items-center justify-center min-h-screen">
-      <SideBarComponent handleOptionSelect={handleOptionSelect} />
+      
+      <div className="admin-container">
+        <SideBarComponent handleOptionSelect={handleOptionSelect} />
 
-     <div className={responsiveFormStyles.container}>
-        {selectedOption === "recetas" && <Recetas />}
-        {selectedOption === "usuarios" && <Usuarios />}
-        {selectedOption === "blog" && <Blog />}
+        <div className="admin-content">
+          {selectedOption === "recetas" && <Recetas />}
+          {selectedOption === "usuarios" && <Usuarios />}
+          {selectedOption === "blog" && <Blog />}
+        </div>
       </div>
     </div>
-  </div>
- 
+
   );
 
-  
+
 }
 
 export default AdminHome;
