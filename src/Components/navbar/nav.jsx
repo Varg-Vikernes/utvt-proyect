feaf0699e6ae5edfe8d9bdea83eef3b74491f21e
Darkneko0 <al221911729@gmail.com>
2023-10-27 15:59:19 -0600

    Feat(Navbar)
    - Se arreglo el problema de animación y lógica
    - Coloco el identificador en recetas

--- src/Components/navbar/nav.jsx
+++ src/Components/navbar/nav.jsx
@@ -1,130 +1,124 @@
-import React from "react";
-import { useNavigate, useLocation, HashRouter, Link } from "react-router-dom";
-import { formStyles } from "../../styles/Constants";
-import { isAuthenticated } from "../../services/authentication/userUtils";
-import { logout } from "../../services/authentication/authUtils";
-import { hasRole } from "../../services/authorization/roleUtils";
-const userDataString = localStorage.getItem("userData"); // Obtiene la cadena JSON de localStorage
-const userData = JSON.parse(userDataString);
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { formStyles } from '../../styles/Constants'
+import { isAuthenticated } from '../../services/authentication/userUtils'
+import { logout } from '../../services/authentication/authUtils'
+import { hasRole } from '../../services/authorization/roleUtils'
+const userDataString = localStorage.getItem('userData') // Obtiene la cadena JSON de localStorage
+const userData = JSON.parse(userDataString)
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
+    const navigate = useNavigate()
+    const location = useLocation()
+    const currentPath = location.hash;
 
-  const userIsLoggedIn = isAuthenticated();
-  const isLoginPage = location.pathname === "/login";
-  const isRegisterPage = location.pathname === "/register";
-  return (
-    <nav className={formStyles.navbar.nav}>
-      <div className={formStyles.navbar.container}>
-        {/* Logo */}
-        <div>
-          <img
-            src="../assets/home/navbar/logo_Q-Spicy.png"
-            alt="Logo de Q-spicy"
-            className={formStyles.navbar.logo}
-          />
-        </div>
+    const userIsLoggedIn = isAuthenticated()
+    const isLoginPage = location.pathname === '/login'
+    const isRegisterPage = location.pathname === '/register'
 
-        {/* Menú de navegación */}
-        <div className={formStyles.navbar.menu}>
-          <NavLink
-            path="/#bienvenida"
-            currentPath={location.pathname + location.hash}
-            onClick={navigate}
-          >
-            Bienvenido
-          </NavLink>
-          <NavLink
-            path="/#como-surgio-esta-idea"
-            currentPath={location.pathname + location.hash}
-            onClick={navigate}
-          >
-            ¿Cómo Surgió?
-          </NavLink>
-          <NavLink
-            path="/#conocenos"
-            currentPath={location.pathname + location.hash}
-            onClick={navigate}
-          >
-            Conócenos
-          </NavLink>
-          <NavLink
-            path="/#recetario"
-            currentPath={location.pathname + location.hash}
-            onClick={navigate}
-          >
-            Recetario
-          </NavLink>
-          <NavLink
-            path="/#blog"
-            currentPath={location.pathname + location.hash}
-            onClick={navigate}
-          >
-            Blog
-          </NavLink>
-        </div>
+    // Agregar un efecto para el desplazamiento suave a la ubicación actual
+    useEffect(() => {
+      // Función para sincronizar la ubicación con el fragmento actual
+      const syncLocationWithFragment = () => {
+        const currentPath = location.hash;
+        if (currentPath && currentPath !== '#' && currentPath[0] === '#') {
+          const targetElement = document.querySelector(currentPath);
+          if (targetElement) {
+            console.log('Sincronizando con fragmento:', currentPath);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+            console.log('Desplazándose a:', currentPath);
+          }
+        }
+      };
+  
+      // Sincronizar la ubicación al cargar la página
+      syncLocationWithFragment();
+    }, [location]);
 
-        {/* Botones de inicio de sesión y registro */}
-        <div className="flex items-center space-x-2">
-          {userIsLoggedIn ? (
-            // Mostrar el botón de Cerrar Sesión si el usuario está autenticado
-            <div className="flex items-center space-x-2">
-              {hasRole(userData, "administrador") && (
-                <TransparentButton onClick={() => navigate("/admin")}>
-                  Administrar
-                </TransparentButton>
-              )}
-              <TransparentButton onClick={() => logout()}>
-                Cerrar Sesión
-              </TransparentButton>
+    return (
+        <nav className={formStyles.navbar.nav}>
+            <div className={formStyles.navbar.container}>
+                {/* Logo */}
+                <div>
+                    <img
+                        src="../assets/home/navbar/logo_Q-Spicy.png"
+                        alt="Logo de Q-spicy"
+                        className={formStyles.navbar.logo}
+                    />
+                </div>
+
+                {/* Menú de navegación */}
+                <div className={formStyles.navbar.menu}>
+                    <NavLink to="#bienvenida">Bienvenido</NavLink>
+                    <NavLink to="#como-surgio-esta-idea">¿Cómo Surgió?</NavLink>
+                    <NavLink to="#conocenos">Conócenos</NavLink>
+                    <NavLink to="#recetario">Recetario</NavLink>
+                    <NavLink to="#blog">Blog</NavLink>
+                </div>
+
+                {/* Botones de inicio de sesión y registro */}
+                <div className="flex items-center space-x-2">
+                    {userIsLoggedIn ? (
+                        // Mostrar el botón de Cerrar Sesión si el usuario está autenticado
+                        <div className="flex items-center space-x-2">
+                            {hasRole(userData, 'administrador') && (
+                                <TransparentButton onClick={() => navigate('/admin')}>
+                                    Administrar
+                                </TransparentButton>
+                            )}
+                            <TransparentButton onClick={() => logout()}>
+                                Cerrar Sesión
+                            </TransparentButton>
+                        </div>
+                    ) : (
+                        // Mostrar botones de Iniciar Sesión y Registrarse si el usuario no está autenticado
+                        <div className="flex items-center space-x-2">
+                            {!isLoginPage && (
+                                <TransparentButton onClick={() => navigate('/login')}>
+                                    Iniciar Sesión
+                                </TransparentButton>
+                            )}
+                            {!isRegisterPage && (
+                                <TransparentButton onClick={() => navigate('/register')}>
+                                    Registrar
+                                </TransparentButton>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
-          ) : (
-            // Mostrar botones de Iniciar Sesión y Registrarse si el usuario no está autenticado
-            <div className="flex items-center space-x-2">
-              {!isLoginPage && (
-                <TransparentButton onClick={() => navigate("/login")}>
-                  Iniciar Sesión
-                </TransparentButton>
-              )}
-              {!isRegisterPage && (
-                <TransparentButton onClick={() => navigate("/register")}>
-                  Registrar
-                </TransparentButton>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
-};
+        </nav>
+    )
+}
 
-// console.log(location.pathname)
-// console.log(location.hash)
-// console.log(location.pathname+location.hash)
-// Componente para los elementos del menú de navegación
-const NavLink = ({ path, currentPath, onClick, children }) => {
-  const isActive = currentPath === path;
-  const linkClasses = isActive
-    ? "text-white active:text-slate-800 hover:text-custom-green border-b-2 border-white rounded-md font-bold text-16"
-    : "text-white hover:border-white hover:border-b-2 rounded-s border-transparent border-b-2 font-bold";
+const NavLink = ({ to,  children }) => {
+    const navigate = useNavigate()
+    const currentPath =window.location.hash;
+    const isActive = currentPath === to
+    console.log(currentPath, to, isActive)
+    const linkClasses = `text-white font-bold text-16 border-transparent hover:text-custom-green ${
+        isActive ? 'border-white' : 'hover:border-white'
+    } rounded-s ${isActive ? 'border-b-2' : 'border-b-0'}`
 
-  return (
-    <a className={linkClasses} onClick={() => onClick(path)}>
-      {children}
-    </a>
-  );
-};
+    const handleClick = () => {
+        navigate(to)
+    }
+
+    return (
+        <a href={to} onClick={handleClick} className={linkClasses}>
+            {children}
+        </a>
+    )
+}
 
 // Componente para los botones transparentes con efecto hover
 const TransparentButton = ({ onClick, children }) => (
-  <button
-    className="bg-transparent text-white active:text-slate-800 hover:border-4 hover:border-white hover:rounded-md p-2 px-4 border-4 border-transparent "
-    onClick={onClick}
-  >
-    {children}
-  </button>
-);
+    <button
+        className="bg-transparent text-white active:text-slate-800 hover:border-4 hover:border-white hover:rounded-md p-2 px-4 border-4 border-transparent "
+        onClick={onClick}>
+        {children}
+    </button>
+)
 
-export default Navbar;
+export default Navbar
