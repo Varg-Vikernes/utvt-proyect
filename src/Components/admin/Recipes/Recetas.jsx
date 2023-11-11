5afd795d46c5c78a03bb185b44233d391352a92e
Valentin Martinez <valentinmartinez1103@gmail.com>
2023-11-10 20:14:32 -0600

    Se implemento la parte para la eliminacion de recetas.

--- src/Components/admin/Recipes/Recetas.jsx
+++ src/Components/admin/Recipes/Recetas.jsx
@@ -39,6 +39,35 @@ function Recetas() {
         openFormEdicion()
     }
 
+    /* Función que envía una solicitud de eliminación a la API cuando el usuario hace clic en el botón de eliminar. Puedes usar el método HTTP DELETE unción que envíe una solicitud de eliminación a la API cuando el usuario haga clic en el botón de eliminar. Puedes usar el método HTTP DELETE  */
+    const handleDeleteClick = async (recetaId) => {
+        try {
+            const token = localStorage.getItem('tokenSession')
+
+            const response = await fetch(
+                `https://backend-proyecto-api-production.up.railway.app/recetas/${recetaId}`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Bearer ${token}`, // Incluye el token en el encabezado de autorización
+                        'Content-Type': 'application/json', // Ajusta según el tipo de contenido que tu backend acepte
+                    },
+                }
+            )
+
+            if (!response.ok) {
+                throw new Error(`Error al eliminar receta: ${response.status}`)
+            }
+
+            // Actualiza el estado solo si la eliminación fue exitosa
+            setRecetas((prevRecetas) =>
+                prevRecetas.filter((receta) => receta.idReceta !== recetaId)
+            )
+        } catch (error) {
+            console.error('Error al eliminar receta:', error)
+        }
+    }
+
     useEffect(() => {
         // Realizar la solicitud a la API usando fetch
         fetch('https://backend-proyecto-api-production.up.railway.app/recetas')
@@ -147,18 +176,19 @@ function Recetas() {
                                 )}
 
                                 <button
+                                    onClick={() => handleDeleteClick(receta.idReceta)}
                                     type="button"
                                     data-modal-toggle="delete-user-modal"
-                                    class="text-white bg-red-600 hover:bg-red-800  font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center">
+                                    className="text-white bg-red-600 hover:bg-red-800 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center">
                                     <svg
-                                        class="mr-2 h-5 w-5"
+                                        className="mr-2 h-5 w-5"
                                         fill="currentColor"
                                         viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             fillRule="evenodd"
                                             d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                            clip-rule="evenodd"></path>
+                                            clipRule="evenodd"></path>
                                     </svg>
                                     Borrar
                                 </button>
